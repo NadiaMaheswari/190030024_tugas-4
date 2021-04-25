a5d1db7414a0a85bfa9e16ff41619e51856ab66f
Taida no Tsumi <Devadrago123@gmail.com>
2021-04-25 10:28:42 +0800

    Stateful Widget Tugas 4

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  //buat method
+  int number = 100;
+  void tekanTombol() {
+    setState(() {
+      number = number - 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Latihan Stateful Widget"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(number.toString()),
+              MaterialButton(
+                color: Colors.amber[100],
+                height: 50,
+                shape: RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(5.0)),
+                onPressed: tekanTombol,
+                child: Text("Berkuranglah"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
